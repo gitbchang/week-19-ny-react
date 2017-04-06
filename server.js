7df952dad34ce26a474821b00e9786de2f0144ac
gitbchang <brian.h.chang1@gmail.com>
2017-04-06 17:12:26 -0500

    merging react router files

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-// Web Scraper Homework Solution Example
-// (be sure to watch the video to see
-// how to operate the site in the browser)
-// -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/
-
-// Require our dependencies
+// Dependencies
 var express = require("express");
-var mongoose = require("mongoose");
-var expressHandlebars = require("express-handlebars");
 var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+// Requiring our Note and Article models
+// var Note = require("./models/Note.js");
+var Article = require("./models/article.js");
+// Our scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
 
 // Set up our port to be either the host's designated port, or 3000
 var PORT = process.env.PORT || 3000;
@@ -15,20 +18,18 @@ var PORT = process.env.PORT || 3000;
 // Instantiate our Express App
 var app = express();
 
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
 // Set up an Express Router
 var router = express.Router();
-
-// Require our routes file pass our router object
-require("./config/routes")(router);
 
 // Designate our public folder as a static directory
 app.use(express.static(__dirname + "/public"));
 
-// Connect Handlebars to our Express app
-app.engine("handlebars", expressHandlebars({
-  defaultLayout: "main"
-}));
-app.set("view engine", "handlebars");
 
 // Use bodyParser in our app
 app.use(bodyParser.urlencoded({
@@ -40,7 +41,7 @@ app.use(router);
 
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var db = process.env.MONGODB_URI || "mongodb://localhost/nytreact";
 
 // Connect mongoose to our database
 mongoose.connect(db, function(error) {
@@ -58,3 +59,5 @@ mongoose.connect(db, function(error) {
 app.listen(PORT, function() {
   console.log("Listening on port:" + PORT);
 });
+
+module.exports = app;
